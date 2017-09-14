2b49e7ca76eee30d2aec2e0b60581f9610fc0652
Manish <manish.mehta@comprotechnologies.com>
2017-09-14 17:15:20 +0530

    Fix for close responsive menu “on click”
    Menu click

--- src/js/main.js
+++ src/js/main.js
@@ -25,8 +25,16 @@ jQuery(document).ready(function ($) {
             }
         }
     });
-	
-	
+
+    $(function(){
+        var navMain = $(".navbar-collapse"); // avoid dependency on #id
+        // "a:not([data-toggle])" - to avoid issues caused
+        // when you have dropdown inside navbar
+        navMain.on("click", "a:not([data-toggle])", null, function () {
+            navMain.collapse('hide');
+        });
+    });
+
 	/*---------------------------------------------*
      * Portfolio Pop Up Animation
      ---------------------------------------------*/
